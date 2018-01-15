84b0ecd11da7ef81a3f827cecc60bdc7f0dffd37
Jan Molak <jan.molak@fil.com>
2018-01-15 15:11:41 +0000

    test(ci): Testing with node 8

--- spec/failsafe.spec.ts
+++ spec/failsafe.spec.ts
@@ -12,7 +12,7 @@ describe(`Failsafe`, function() {
     const 
         Success = 0,
         General_Failure = 1,
-        Fatal_Error = 2;
+        Fatal_Error = 10;
 
     let logger: AccumulatingLogger,
         failsafe: Failsafe;
